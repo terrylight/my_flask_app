986ae3ca9fe1e653fb5707feee4649502e924eec
Teresia <true@terrylight.org>
2025-04-30 23:33:29 +0300

    Remove node_modules and next build files from repo

--- app.py
+++ app.py
@@ -152,3 +152,8 @@ def logout():
 # Running the application with Waitress
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5001)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use PORT if set, else 5000
+    app.run(host="0.0.0.0", port=port)
